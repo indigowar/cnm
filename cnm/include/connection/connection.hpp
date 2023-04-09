2dd40fe7da54aad0c9c4c55e5085d8e5f174d3a9
Indigowar <indigowar.github@gmail.com>
2023-04-09 16:36:31 +0300

    add connection.hpp(empty)

---
+++ cnm/include/connection/connection.hpp
@@ -0,0 +1,14 @@
+#ifndef HPP_CNM_LIB_CONNECTION_CONNECTION_HPP
+#define HPP_CNM_LIB_CONNECTION_CONNECTION_HPP
+
+namespace cnm::connection {
+
+/**
+ * This class is the main connection class,
+ * it manages the transferring messages between machines
+ */
+class Connection {};
+
+}  // namespace cnm::connection
+
+#endif  // HPP_CNM_LIB_CONNECTION_CONNECTION_HPP
