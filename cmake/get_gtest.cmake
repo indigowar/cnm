2f069f74f9a67d94208ea25077cbc42faf5a2615
Indigowar <indigowar.github@gmail.com>
2023-04-09 16:17:36 +0300

    initial

---
+++ cmake/get_gtest.cmake
@@ -0,0 +1,7 @@
+include(FetchContent)
+FetchContent_Declare(
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG 58d77fa8070e8cec2dc1ed015d66b454c8d78850 # release-1.12.1
+  OVERRIDE_FIND_PACKAGE)
+FetchContent_MakeAvailable(googletest)
---
+++ cnm/include/cnm.hpp
@@ -0,0 +1,9 @@
+#ifndef HPP_CNM_CNM_LIB_CNM_HPP
+#define HPP_CNM_CNM_LIB_CNM_HPP
+
+namespace cnm
+{
+
+}
+
+#endif // HPP_CNM_CNM_LIB_CNM_HPP
