de3ef20cd10a1b2e167a4a50998a3564804d09c3
Indigowar <indigowar.github@gmail.com>
2023-04-20 14:09:43 +0300

    wrote the documentation for connection context class

--- cnm/include/communication/context.hpp
+++ cnm/include/communication/context.hpp
@@ -9,16 +9,41 @@ namespace cnm::communication {
 
 class connection;
 
+/**
+ * @brief the main class of connection's context
+ *
+ * @details
+ * This class hides all internal work with connection classes.
+ * It simplifies it to just read and write operations and abort call.
+ *
+ */
 class context {
  public:
+  /**
+   *
+   * @param con - connection
+   * @param id  - given to this context id
+   */
   context(connection& con, int id);
 
   virtual ~context() = default;
 
+  /**
+   * @brief calling connection::abort()
+   *
+   * @see connection
+   */
   void abort();
 
+  /**
+   * @brief write message to the connection
+   */
   void write(message&&);
 
+  /**
+   * @brief reads a message
+   * @return a received message
+   */
   message read();
 
   context(const context&) = delete;
@@ -28,7 +53,14 @@ class context {
   context& operator=(context&&) = delete;
 
  protected:
+  /**
+   * @return checks is context can write multiple messages to it
+   */
   virtual bool buffered_write_channel_check(connection&) = 0;
+
+  /**
+   * @return checks is context can read multiple messages to it
+   */
   virtual bool buffered_read_channel_check(connection&) = 0;
 
  private:
