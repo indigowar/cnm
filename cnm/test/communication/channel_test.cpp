799a8b521f8d98ba45e7ac99bb4d3ede0dbcfa21
Indigowar <indigowar.github@gmail.com>
2023-05-11 21:21:07 +0300

    added test for writing and reading from the one-time channel

--- cnm/test/communication/channel_test.cpp
+++ cnm/test/communication/channel_test.cpp
@@ -4,16 +4,28 @@
 
 using namespace cnm::communication;
 
-TEST(CreationOneMessageChannel, ConstructorCreatesChannelRight) {
+TEST(ChannelTest, ConstructorCreatesOnceChannelRight) {
   auto once = channel_t<int, 1>();
 
   EXPECT_EQ(once.limit(), 1);
   EXPECT_FALSE(once.is_closed());
 }
 
-TEST(CreationMultipleMessageChannel, ConstructorCreatesChannelRight) {
+TEST(ChannelTest, ConstructorCreatesMultipleChannelRight) {
   auto multiple_channel = channel_t<int, 50>();
 
   EXPECT_EQ(multiple_channel.limit(), 50);
   EXPECT_FALSE(multiple_channel.is_closed());
+}
+
+TEST(ChannelTest, WritingAndReadingInOnceChannel) {
+  channel_t<int, 1> chan{};
+
+  const int data = 500;
+
+  chan.write(data);
+
+  auto result = chan.read().get();
+
+  EXPECT_EQ(data, result);
 }
