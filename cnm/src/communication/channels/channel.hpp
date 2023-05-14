c368c4184a81fe7d797e04d82ff8c7538e7bcf0a
Indigowar <indigowar.github@gmail.com>
2023-05-14 10:36:55 +0300

    added channel_is_overflowed in channel

--- cnm/src/communication/channels/channel.hpp
+++ cnm/src/communication/channels/channel.hpp
@@ -15,7 +15,7 @@ class channel final {
 
   class closed_channel_error : public std::exception {};
 
-  class overflowed_channel_error : public std::exception {};
+  struct channel_is_overflowed : public std::exception {};
 
   static channel<package_t> make_with_limit(size_t limit_value);
 
@@ -35,48 +35,6 @@ class channel final {
   channel<package_t>& operator>>(package_t& value) {}
 
  private:
-  struct storage_t {
-    virtual ~storage_t() = default;
-    virtual void push(package_t) = 0;
-    virtual std::future<package_t> pop() = 0;
-    virtual size_t capacity() const noexcept = 0;
-    virtual size_t current() const noexcept = 0;
-  };
-
-  struct buffered_storage_t : public storage_t {
-   public:
-    explicit buffered_storage_t(size_t max_size) : m_max_size(max_size) {}
-
-    void push(package_t package) override {
-      std::unique_lock lock(m_mutex);
-      if (m_list.size() >= m_max_size) {
-        throw overflowed_channel_error();
-      }
-      std::promise<package_t> promise;
-      m_list.push_back(std::move(promise.get_future()));
-      promise.set_value(package);
-    }
-
-    std::future<package_t> pop() override {}
-
-    size_t capacity() const noexcept override {
-      std::shared_lock lock(m_mutex);
-      return m_max_size;
-    }
-
-    size_t current() const noexcept override {
-      std::shared_lock lock(m_mutex);
-      return m_list.size();
-    }
-
-   private:
-    std::list<std::future<package_t>> m_list;
-    std::list<std::promise<package_t>> m_packages;
-    size_t m_max_size;
-    std::shared_mutex m_mutex;
-  };
-
-  struct unbuffered_storage_t : public storage_t {};
 };
 
 }  // namespace cnm::communication
