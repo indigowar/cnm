aba8abc87b1a567b490715391ca2d7fb2e0c9f5e
Max Kamerov <indigowar.github@gmail.com>
2023-07-26 15:22:43 +0300

    Made refactoring for CMake files.

--- cmake/get_imgui.cmake
+++ cmake/get_imgui.cmake
@@ -3,47 +3,52 @@
 find_package(Vulkan REQUIRED FATAL_ERROR)
 
 FetchContent_Declare(
-        glad
-        GIT_REPOSITORY https://github.com/Dav1dde/glad.git
-        GIT_TAG df8e9e16110b305479a875399cee13daa0ccadd9
-)
+  glad
+  GIT_REPOSITORY https://github.com/Dav1dde/glad.git
+  GIT_TAG df8e9e16110b305479a875399cee13daa0ccadd9)
+
 FetchContent_MakeAvailable(glad)
 
-set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
-set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
-set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-set(GLFW_INSTALL OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_DOCS
+    OFF
+    CACHE BOOL "" FORCE)
+set(GLFW_BUILD_EXAMPLES
+    OFF
+    CACHE BOOL "" FORCE)
+set(GLFW_BUILD_TESTS
+    OFF
+    CACHE BOOL "" FORCE)
+set(GLFW_INSTALL
+    OFF
+    CACHE BOOL "" FORCE)
 
 FetchContent_Declare(
-        glfw
-        GIT_REPOSITORY https://github.com/glfw/glfw.git
-        GIT_TAG 3.3.2
-)
+  glfw
+  GIT_REPOSITORY https://github.com/glfw/glfw.git
+  GIT_TAG 3.3.2)
 FetchContent_MakeAvailable(glfw)
 
 FetchContent_Declare(
-        imgui
-        GIT_REPOSITORY https://github.com/ocornut/imgui.git
-        GIT_TAG docking
-)
+  imgui
+  GIT_REPOSITORY https://github.com/ocornut/imgui.git
+  GIT_TAG docking)
 
 FetchContent_GetProperties(imgui)
 
-if (NOT imgui_POPULATED)
-    FetchContent_Populate(imgui)
-    set(IMGUI_INCLUDE_DIR ${imgui_SOURCE_DIR}/)
-    set(IMGUI_BACKENDS
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
-
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_vulkan.h
-            ${imgui_SOURCE_DIR}/backends/imgui_impl_vulkan.cpp
-            )
-    file(GLOB IMGUI_SOURCES ${imgui_SOURCE_DIR}/*.cpp)
-    file(GLOB IMGUI_HEADERS ${imgui_SOURCE_DIR}/*.h)
-    add_library(imgui STATIC ${IMGUI_SOURCES} ${IMGUI_SOURCES} ${IMGUI_BACKENDS})
-    add_definitions(-DIMGUI_IMPL_OPENGL_LOADER_GLAD)
-    target_include_directories(imgui PUBLIC ${IMGUI_INCLUDE_DIR} ${OPENGL_INCLUDE_DIR} ${GLFW_INCLUDE_DIR} ${GLAD_INCLUDE_DIR})
-    target_link_libraries(imgui ${OPENGL_LIBRARIES} glfw glad)
-endif ()
-
+if(NOT imgui_POPULATED)
+  FetchContent_Populate(imgui)
+  set(IMGUI_INCLUDE_DIR ${imgui_SOURCE_DIR}/)
+  set(IMGUI_BACKENDS
+      ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h
+      ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
+      ${imgui_SOURCE_DIR}/backends/imgui_impl_vulkan.h
+      ${imgui_SOURCE_DIR}/backends/imgui_impl_vulkan.cpp)
+  file(GLOB IMGUI_SOURCES ${imgui_SOURCE_DIR}/*.cpp)
+  file(GLOB IMGUI_HEADERS ${imgui_SOURCE_DIR}/*.h)
+  add_library(imgui STATIC ${IMGUI_SOURCES} ${IMGUI_SOURCES} ${IMGUI_BACKENDS})
+  add_definitions(-DIMGUI_IMPL_OPENGL_LOADER_GLAD)
+  target_include_directories(
+    imgui PUBLIC ${IMGUI_INCLUDE_DIR} ${OPENGL_INCLUDE_DIR} ${GLFW_INCLUDE_DIR}
+                 ${GLAD_INCLUDE_DIR})
+  target_link_libraries(imgui ${OPENGL_LIBRARIES} glfw glad)
+endif()
