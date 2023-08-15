5896a7547575843fcf9b71b3d590045e3de3462e
Max Kamerov <indigowar.github@gmail.com>
2023-08-15 15:03:50 +0300

    Added to HostInfo comparison operators.

--- cnm_core/src/cnm/machine/host_info.hpp
+++ cnm_core/src/cnm/machine/host_info.hpp
@@ -2,11 +2,18 @@
 #define HPP_CNM_CORE_MACHINE_HOST_INFO_HPP
 
 #include <string_view>
+
 namespace Cnm {
 
 struct HostInfo final {
   std::string_view name;
   std::string_view address;
+
+  bool operator==(const HostInfo& other) const {
+    return name == other.name && address == other.address;
+  }
+
+  bool operator!=(const HostInfo& other) const { return !(*this == other); }
 };
 
 }  // namespace Cnm
