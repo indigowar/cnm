d2b4265078578c40058b36756c668e94311a4734
Max Kamerov <indigowar.github@gmail.com>
2023-08-20 16:42:36 +0300

    Made Cnm::RingNode.

---
+++ cnm_core/src/cnm/topology/ring/ring_node.cpp
@@ -0,0 +1,74 @@
+#include "ring_node.hpp"
+
+namespace Cnm {
+
+RingNode::RingNode(Cnm::HostInfo host_info, std::unique_ptr<Machine>&& m,
+                   const std::shared_ptr<Communicator>& communicator)
+    : machine{std::move(m)}, previous_node{}, next_node{} {
+  machine->setHostInfo(host_info);
+  machine->setCommunicator(communicator);
+}
+
+RingNode::~RingNode() = default;
+
+HostInfo RingNode::getHostInfo() const noexcept {
+  return machine->getHostInfo();
+}
+
+void RingNode::setHostInfo(HostInfo host_info) {
+  machine->setHostInfo(host_info);
+}
+
+std::vector<std::shared_ptr<Node>> RingNode::getConnectedNodes()
+    const noexcept {
+  std::unique_lock lock(mutex);
+
+  if (next_node && previous_node) {
+    return {next_node, previous_node};
+  }
+
+  if (next_node) {
+    return {next_node};
+  }
+
+  if (previous_node) {
+    return {previous_node};
+  }
+
+  return {};
+}
+
+bool RingNode::isBusy() const noexcept {
+  return machine->getServingLimit() == machine->getCurrentServingAmount();
+}
+
+bool RingNode::isServing() const noexcept {
+  return machine->getCurrentServingAmount() != 0;
+}
+
+std::shared_ptr<RingNode> RingNode::getPreviousNode() const noexcept {
+  std::unique_lock lock(mutex);
+  return previous_node;
+}
+
+void RingNode::setPreviousNode(std::shared_ptr<RingNode> previous) {
+  std::unique_lock lock(mutex);
+  previous_node = std::move(previous);
+}
+
+void RingNode::setNextNode(std::shared_ptr<RingNode> next) {
+  std::unique_lock lock(mutex);
+  next_node = std::move(next);
+}
+
+std::shared_ptr<RingNode> RingNode::getNextNode() const noexcept {
+  std::unique_lock lock(mutex);
+  return next_node;
+}
+
+void RingNode::start() { machine->start(); }
+void RingNode::stop() { machine->stop(); }
+void RingNode::invoke() { machine->invoke(); }
+void RingNode::freeze() { machine->freeze(); }
+
+}  // namespace Cnm
---
+++ cnm_core/src/cnm/topology/ring/ring_node.hpp
@@ -0,0 +1,55 @@
+#ifndef HPP_CNM_CORE_TOPOLOGY_RING_RING_NODE_HPP
+#define HPP_CNM_CORE_TOPOLOGY_RING_RING_NODE_HPP
+
+#include <memory>
+#include <mutex>
+
+#include "cnm/machine/communicator.hpp"
+#include "cnm/machine/machine.hpp"
+#include "cnm/topology/base/node.hpp"
+
+namespace Cnm {
+
+class RingNode final : public Node {
+ public:
+  RingNode(HostInfo, std::unique_ptr<Machine>&&,
+           const std::shared_ptr<Communicator>&);
+
+  ~RingNode() override;
+
+  [[nodiscard]] HostInfo getHostInfo() const noexcept override;
+
+  void setHostInfo(HostInfo) override;
+
+  std::vector<std::shared_ptr<Node>> getConnectedNodes()
+      const noexcept override;
+
+  bool isServing() const noexcept override;
+
+  bool isBusy() const noexcept;
+
+  [[nodiscard]] std::shared_ptr<RingNode> getPreviousNode() const noexcept;
+
+  void setPreviousNode(std::shared_ptr<RingNode>);
+
+  [[nodiscard]] std::shared_ptr<RingNode> getNextNode() const noexcept;
+
+  void setNextNode(std::shared_ptr<RingNode>);
+
+  void start() override;
+  void stop() override;
+  void invoke() override;
+  void freeze() override;
+
+ private:
+  std::shared_ptr<RingNode> previous_node;
+  std::shared_ptr<RingNode> next_node;
+
+  std::unique_ptr<Machine> machine;
+
+  mutable std::mutex mutex;
+};
+
+}  // namespace Cnm
+
+#endif  // HPP_CNM_CORE_TOPOLOGY_RING_RING_NODE_HPP
