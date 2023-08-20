8cd0853da7539457e46117775ef5a6230bf822c3
Max Kamerov <indigowar.github@gmail.com>
2023-08-20 20:43:56 +0300

    Implemented RingIterator

--- cnm_core/src/cnm/topology/ring/ring_iterator.hpp
+++ cnm_core/src/cnm/topology/ring/ring_iterator.hpp
@@ -1,35 +1,36 @@
-//#ifndef HPP_CNM_CORE_RING_RING_ITERATOR_HPP
-//#define HPP_CNM_CORE_RING_RING_ITERATOR_HPP
-//
-//#include <memory>
-//
-//#include "cnm/topology/base/node.hpp"
-//#include "cnm/topology/base/node_iterator.hpp"
-//#include "cnm/topology/ring/ring_node.hpp"
-//
-//namespace Cnm {
-//
-//class RingIterator final : public NodeIterator {
-// public:
-//  explicit RingIterator(std::shared_ptr<RingNode> node)
-//      : node{std::move(node)} {}
-//
-//  std::shared_ptr<Node> operator*() const override { return node; }
-//
-//  NodeIterator& operator++() override {
-//    node = std::move(node->getNextNode());
-//    return *this;
-//  }
-//
-//  NodeIterator& operator--() override {
-//    node = std::move(node->getPreviousNode());
-//    return *this;
-//  }
-//
-// private:
-//  std::shared_ptr<RingNode> node;
-//};
-//
-//}  // namespace Cnm
-//
-//#endif  // HPP_CNM_CORE_RING_RING_ITERATOR_HPP
+#ifndef HPP_CNM_CORE_TOPOLOGY_RING_RING_ITERATOR_HPP
+#define HPP_CNM_CORE_TOPOLOGY_RING_RING_ITERATOR_HPP
+
+#include "cnm/topology/base/node_iterator.hpp"
+#include "cnm/topology/ring/ring_node.hpp"
+
+namespace Cnm {
+
+class RingIterator final : public NodeIterator {
+ public:
+  explicit RingIterator(std::shared_ptr<RingNode> node)
+      : node{std::move(node)} {}
+
+  std::shared_ptr<Node> operator*() const override { return node; }
+
+  NodeIterator& operator++() override {
+    if (node) {
+      node = node->getNextNode();
+    }
+    return *this;
+  }
+
+  NodeIterator& operator--() {
+    if (node) {
+      node = node->getPreviousNode();
+    }
+    return *this;
+  }
+
+ private:
+  std::shared_ptr<RingNode> node;
+};
+
+}  // namespace Cnm
+
+#endif  // HPP_CNM_CORE_TOPOLOGY_RING_RING_ITERATOR_HPP
