991bc047f5911ee0083e6fb43218c938b5c3b3b6
Max Kamerov <indigowar.github@gmail.com>
2023-08-31 21:43:23 +0300

    Adder ReverseIterator.

--- cnm_core/src/cnm/topology/ring/ring_iterator.hpp
+++ cnm_core/src/cnm/topology/ring/ring_iterator.hpp
@@ -63,6 +63,62 @@ class RingIterator final {
   std::shared_ptr<RingNode> node;
 };
 
+class RingReverseIterator final {
+ public:
+  explicit RingReverseIterator(std::shared_ptr<RingNode> node)
+      : node{std::move(node)} {}
+
+  ~RingReverseIterator() = default;
+
+  RingReverseIterator(const RingReverseIterator&) = default;
+  RingReverseIterator& operator=(const RingReverseIterator&) = default;
+
+  RingReverseIterator(RingReverseIterator&&) = default;
+  RingReverseIterator& operator=(RingReverseIterator&&) = default;
+
+  std::shared_ptr<Node> operator*() const noexcept { return node; }
+
+  RingReverseIterator& operator++() {
+    if (node) {
+      node = node->getNextNode();
+    }
+    return *this;
+  }
+
+  RingReverseIterator& operator--() {
+    if (node) {
+      node = node->getPreviousNode();
+    }
+    return *this;
+  }
+
+  template <NodeIterator I>
+  bool operator==(const I& other) const {
+    auto t = this->operator*();
+    auto o = this->operator*();
+
+    // equal pointers - same node.
+    if (t == o) {
+      return true;
+    }
+
+    // if one of pointers is nullptr and other one is not.
+    if (!t || !o) {
+      return false;
+    }
+
+    return t->getHostInfo() == o->getHostInfo();
+  }
+
+  template <NodeIterator I>
+  bool operator!=(const I& o) const {
+    return !this->operator==(o);
+  }
+
+ private:
+  std::shared_ptr<RingNode> node;
+};
+
 }  // namespace Cnm
 
 #endif  // HPP_CNM_CORE_TOPOLOGY_RING_RING_ITERATOR_HPP
