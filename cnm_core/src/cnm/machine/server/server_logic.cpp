0277fafc2b422b0fc7ac7e864ea167e5bfe4f8da
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2023-09-17 21:00:49 +0300

    feat: made FileServerLogic

---
+++ cnm_core/src/cnm/machine/server/server_logic.cpp
@@ -0,0 +1,69 @@
+#include "server_logic.hpp"
+
+#include <spdlog/spdlog.h>
+
+namespace Cnm {
+
+FileServerLogic::FileServerLogic(std::map<std::string, std::string> df)
+    : default_files(std::move(df)), file_storage() {
+  file_storage.insert(default_files.begin(), default_files.end());
+}
+
+void FileServerLogic::init() {
+  file_storage.clear();
+  file_storage.insert(default_files.begin(), default_files.end());
+}
+
+void FileServerLogic::execute(ServerCtx&& ctx) {
+  auto request = readRequest(ctx);
+  if (request.isErr()) {
+    spdlog::warn("Got an error, while reading the request: %s",
+                 request.unwrapErr());
+    ctx->abort();
+    return;
+  }
+  auto body = request.unwrap();
+
+  if (body.containsOneMessage()) {
+    retrieveFile(std::move(ctx), body.getMessage());
+  } else {
+    retrieveFiles(std::move(ctx), body);
+  }
+}
+
+result_t<MessageBatch> FileServerLogic::readRequest(ServerCtx& ctx) {
+  auto future = ctx->acceptRequest();
+  future.wait();
+  return future.get();
+}
+
+void FileServerLogic::retrieveFile(ServerCtx&& ctx, const Message& file_name) {
+  std::unique_lock lock(mutex);
+  if (!file_storage.contains(file_name)) {
+    ctx->sendResponse(MessageBatch(Message("file is not found")));
+    return;
+  }
+  auto file = file_storage.at(file_name);
+  ctx->sendResponse(MessageBatch(Message(file)));
+}
+
+void FileServerLogic::retrieveFiles(ServerCtx&& ctx,
+                                    const MessageBatch& request) {
+  std::unique_lock lock(mutex);
+  std::vector<Message> files{};
+
+  auto file_names = request.getMessageList();
+
+  std::for_each(file_names.begin(), file_names.end(),
+                [this, &files](const auto& i) {
+                  if (!file_storage.contains(i)) {
+                    files.emplace_back("not found");
+                  } else {
+                    files.emplace_back(file_storage.at(i));
+                  }
+                });
+
+  ctx->sendResponse(MessageBatch(std::move(files)));
+}
+
+}  // namespace Cnm
--- cnm_core/src/cnm/machine/server/server_logic.hpp
+++ cnm_core/src/cnm/machine/server/server_logic.hpp
@@ -1,6 +1,11 @@
 #ifndef HPP_CNM_CORE_MACHINE_SERVER_SERVER_LOGIC_HPP
 #define HPP_CNM_CORE_MACHINE_SERVER_SERVER_LOGIC_HPP
 
+#include <map>
+#include <string>
+
+#include "cnm/connection/server_ctx.hpp"
+
 namespace Cnm {
 
 class ServerLogic {
@@ -10,6 +15,27 @@ class ServerLogic {
   virtual void init() {}
 
   virtual void execute(ServerCtx&&) {}
+};
+
+class FileServerLogic final : public ServerLogic {
+ public:
+  explicit FileServerLogic(std::map<std::string, std::string>);
+
+  void init() override;
+
+  void execute(ServerCtx&&) override;
+
+ private:
+  static result_t<MessageBatch> readRequest(ServerCtx& ctx);
+
+  void retrieveFile(ServerCtx&& ctx, const Message& file_name);
+
+  void retrieveFiles(ServerCtx&& ctx, const MessageBatch&);
+
+  std::map<std::string, std::string> file_storage;
+  std::map<std::string, std::string> default_files;
+
+  std::mutex mutex;
 };
 
 }  // namespace Cnm
