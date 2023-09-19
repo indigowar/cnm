452fc871e9d7f2fad412197dbd6ef6a74a1420e0
Max Kamerov <57366917+indigowar@users.noreply.github.com>
2023-09-19 15:02:55 +0300

    feat: Added Object's methods for Machine's Mock.

--- cnm_core/test/mocks/machine.hpp
+++ cnm_core/test/mocks/machine.hpp
@@ -7,6 +7,16 @@
 
 class MockMachine : public Cnm::Machine {
  public:
+  MOCK_METHOD(void, start, (), (override));
+
+  MOCK_METHOD(void, stop, (), (override));
+
+  MOCK_METHOD(void, invoke, (), (override));
+
+  MOCK_METHOD(void, freeze, (), (override));
+
+  MOCK_METHOD(Object::Status, getStatus, (), (const, noexcept, override));
+
   MOCK_METHOD(std::string_view, getType, (), (const, noexcept));
 
   MOCK_METHOD(size_t, getServingLimit, (), (const, noexcept));
