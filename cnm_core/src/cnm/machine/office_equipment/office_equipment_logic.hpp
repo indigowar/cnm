03aa4caf24d71cf1a9c695e088c6a80f13bf8a9e
Max Kamerov <indigowar.github@gmail.com>
2023-10-15 21:00:06 +0300

    refactor: cleanup office_equipment_logic.[hpp,cpp]

--- cnm_core/src/cnm/machine/office_equipment/office_equipment_logic.hpp
+++ cnm_core/src/cnm/machine/office_equipment/office_equipment_logic.hpp
@@ -5,40 +5,6 @@
 
 #include "cnm/connection/server_ctx.hpp"
 
-// namespace Cnm {
-//
-// class OfficeEquipmentLogic {
-//  public:
-//   virtual ~OfficeEquipmentLogic() = default;
-//
-//   virtual void init() {}
-//
-//   virtual void execute(ServerCtx&&) {}
-// };
-//
-//// ScannerLogic - the logic of the scanner.
-//// For simplicity, it reads a file from the filesystem and returns the content
-//// of the file.
-// class [[maybe_unused]] ScannerLogic : public OfficeEquipmentLogic {
-//  public:
-//   void init() override;
-//
-//   void execute(ServerCtx&&) override;
-//
-//  private:
-//   static std::string readFile(const std::string& name);
-// };
-//
-//// PrinterLogic - the logic of the printer.
-//// For simplicity, it writes into the filesystem.
-// class [[maybe_unused]] PrinterLogic : public OfficeEquipmentLogic {
-//  public:
-//   void init() override;
-//   void execute(ServerCtx&&) override;
-// };
-//
-// }  // namespace Cnm
-
 namespace Cnm {
 
 // OfficeEquipmentLogic - a stateless server of requests.
